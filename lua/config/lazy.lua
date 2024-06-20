7f283c2cf735efedd08a75a6380a4b01566d22d5
samaelod <s.mailoud90@gmail.com>
2024-06-21 00:44:12 +0200

    Improving

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,8 +31,6 @@ require("lazy").setup({
 	--
 	--  This is equivalent to:
 	--    require('Comment').setup({})
-
-	-- { 'Exafunction/codeium.vim', version = '1.8.37' },
 	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
 
@@ -552,23 +550,58 @@ require("lazy").setup({
 		end,
 	},
 
-	-- { -- You can easily change to a different colorscheme.
-	-- 	-- Change the name of the colorscheme plugin below, and then
-	-- 	-- change the command in the config to whatever the name of that colorscheme is.
-	-- 	--
-	-- 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	-- 	"rebelot/kanagawa.nvim",
-	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
-	-- 	init = function()
-	-- 		-- Load the colorscheme here.
-	-- 		-- Like many other themes, this one has different styles, and you could load
-	-- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-	-- 		vim.cmd.colorscheme("catpuccin")
-	--
-	-- 		-- You can configure highlights by doing something like:
-	-- 		vim.cmd.hi("Comment gui=none")
-	-- 	end,
-	-- },
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			term_colors = true,
+			transparent_background = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#102030",
+					crust = "#50B0C0",
+				},
+			},
+			integrations = {
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+				dropbar = {
+					enabled = true,
+					color_mode = true,
+				},
+			},
+		},
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("catppuccin")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
 
 	-- Highlight todo, notes, etc in comments
 	{
--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,32 +1,23 @@
 return {
-
-	"Exafunction/codeium.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
-	},
-	event = "BufEnter",
+	"Exafunction/codeium.vim",
 	version = "1.8.37",
-	-- NOTE: codeium plugin key bindings
-	--set statusline+=%\{...\}%3{codeium#GetStatusString()}
-
 	config = function()
-		-- require('codeium').setup {}
+		-- require("codeium").setup({})
 		-- Add to statusline
 		-- vim.opt.statusline .. '{...}%3{codeium#GetStatusString()}'
 		-- Change '<C-g>' here to any keycode you like.
 		vim.g.codeium_disable_bindings = 1
 		--
-		vim.keymap.set("i", "<A-l>", function()
+		vim.keymap.set("i", "<A-y>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<A-j>", function()
+		vim.keymap.set("i", "<A-n>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<A-k>", function()
+		vim.keymap.set("i", "<A-p>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<A-h>", function()
+		vim.keymap.set("i", "<A-x>", function()
 			return vim.fn["codeium#Clear"]()
 		end, { expr = true, silent = true })
 		--vim.keymap.del('i', '<tab>')
