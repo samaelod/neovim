82c46e1aadd643957011c7d24390e6f8e481fcd5
samaelod <s.mailoud90@gmail.com>
2025-01-22 00:34:15 +0100

    Dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
 	opts = {
 		bigfile = { enabled = true },
 		git = { enabled = true },
+		input = { enabled = true },
 		lazygit = {
 			-- automatically configure lazygit to use the current colorscheme
 			-- and integrate edit with the current neovim instance
@@ -41,17 +42,32 @@ return {
 		dashboard = {
 			enabled = true,
 			sections = {
+				{ section = "header" },
 				{
 					pane = 2,
-					{ section = "keys", gap = 1, padding = 1 },
-					{ section = "startup" },
-				},
-				{
 					section = "terminal",
-					cmd = "chafa ~/Pictures/Eva.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
-					height = 17,
+					cmd = "colorscript -e square",
+					height = 5,
 					padding = 1,
 				},
+				{ section = "keys", gap = 1, padding = 1 },
+				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
+				{ section = "startup" },
 			},
 		},
 		notifier = {
@@ -85,6 +101,50 @@ return {
 		styles = {
 			notification = {
 				wo = { wrap = true }, -- Wrap notifications
+			},
+			blame_line = {
+				width = 0.6,
+				height = 0.6,
+				border = "rounded",
+				title = " Git Blameeee ",
+				title_pos = "center",
+				ft = "git",
+			},
+			input = {
+				backdrop = true,
+				position = "float",
+				border = "rounded",
+				title_pos = "center",
+				height = 1,
+				width = 60,
+				relative = "editor",
+				noautocmd = true,
+				row = 2,
+				-- relative = "cursor",
+				-- row = -3,
+				-- col = 0,
+				wo = {
+					winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+					cursorline = false,
+				},
+				bo = {
+					filetype = "snacks_input",
+					buftype = "prompt",
+				},
+				--- buffer local variables
+				b = {
+					completion = false, -- disable blink completions in input
+				},
+				keys = {
+					n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
+					i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
+					i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = "i", expr = true },
+					i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
+					i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
+					i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
+					i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
+					q = "cancel",
+				},
 			},
 		},
 	},
